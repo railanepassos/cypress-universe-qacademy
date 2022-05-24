63840d5304eeccbd8b597ffdf96a0729cd934e94
Railane Passos <railanepassos@gmail.com>
2022-05-23 22:42:12 -0300

    Indentação do Código

--- training-wheels/cypress/integration/home.spec.js
+++ training-wheels/cypress/integration/home.spec.js
@@ -2,10 +2,13 @@ it('deve exibir a home page', () => {
     cy.visit('https://training-wheels-qaninja.herokuapp.com/');
 
     // Validação pelo titulo da pagina (menos recomendado, precisamos mpensar melhor em nossa estratégia de validação)
-    cy.title().should('eq', 'Training Wheels | QAninja');
+    cy.title()
+        .should('eq', 'Training Wheels | QAninja');
 
     // Validação por elementos da pagina (verificar a existencia de um elemento seria uma melhor estratégia)
-    cy.get('img[alt="QAninja"]').should('be.visible');
-    cy.get('ul.menu-list').should('be.visible');
+    cy.get('img[alt="QAninja"]')
+        .should('be.visible');
+    cy.get('ul.menu-list')
+        .should('be.visible');
 
 });
