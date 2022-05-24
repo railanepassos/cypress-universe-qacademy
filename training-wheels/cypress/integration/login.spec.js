eea246d8ff4e86795e6759f99b2f7f158b68d031
Railane Passos <railanepassos@gmail.com>
2022-05-23 22:51:35 -0300

    Aula: Técnicas para buscar e clicar em botões

--- training-wheels/cypress/integration/login.spec.js
+++ training-wheels/cypress/integration/login.spec.js
@@ -1,7 +1,11 @@
 it('deve logar com sucesso', () => {
     cy.visit('https://training-wheels-qaninja.herokuapp.com/login');
 
+    // Preenchendo campos de texto
     cy.get('#nickId').type('papitorocks');
     cy.get('#passId').type('pwd123');
+
+    // Técnicas para buscar e clicar em botões
+    cy.get('button[type="submit"]').click();
 
 });
