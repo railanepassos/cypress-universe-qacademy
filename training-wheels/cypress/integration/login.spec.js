7a8f96a95a5da6f8558ca4958aab4d59c19005ed
Railane Passos <railanepassos@gmail.com>
2022-05-23 22:43:11 -0300

    Preenchendo campos de texto

---
+++ training-wheels/cypress/integration/login.spec.js
@@ -0,0 +1,7 @@
+it('deve logar com sucesso', () => {
+    cy.visit('https://training-wheels-qaninja.herokuapp.com/login');
+
+    cy.get('#nickId').type('papitorocks');
+    cy.get('#passId').type('pwd123');
+
+});
