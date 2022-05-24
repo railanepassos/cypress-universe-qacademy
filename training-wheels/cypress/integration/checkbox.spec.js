b77758d4a4f8fd37a6b1994beffe8cea9a0d8679
Railane Passos <railanepassos@gmail.com>
2022-05-23 23:19:52 -0300

    Aula: Manipulando Checkbox

---
+++ training-wheels/cypress/integration/checkbox.spec.js
@@ -0,0 +1,19 @@
+it('deve marcar os top 5 filmes da marvel', () => {
+    cy.visit('https://training-wheels-qaninja.herokuapp.com/checkboxes');
+
+    const movies = [
+        'blackpanther',
+        'guardians',
+        'strange',
+        'shangchi',
+        'eternals'
+    ]
+
+    movies.forEach(function (movie) {
+        cy.get(`input[name=${movie}]`)
+            .click()
+            .should('be.checked');
+
+    })
+    
+});
