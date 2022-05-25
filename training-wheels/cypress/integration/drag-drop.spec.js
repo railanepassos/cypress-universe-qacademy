f1f0813e4f19754251473a6f07c2c81298cebdd9
Railane Passos <railanepassos@gmail.com>
2022-05-24 22:40:57 -0300

    Aula: Drag And Drop com Ganchos

--- training-wheels/cypress/integration/drag-drop.spec.js
+++ training-wheels/cypress/integration/drag-drop.spec.js
@@ -1,25 +1,30 @@
-it('deve arrastar o cypress para a caixinha do node', () => {
-    cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop');
+describe('drag and drop with hooks', () => {
+    before(() => {
+        cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop');
 
-    // DataTranfer vai ajudar o Cypress a simular a ação de arrastar e soltar
-    const dataTransfer = new DataTransfer();
+    });
+    it('deve arrastar o cypress para a caixinha do node', () => {
 
-    cy.get('img[alt="Cypress"]')
-        .trigger('dragstart', { dataTransfer });
-    cy.get('.nodejs figure[draggable="true"]')
-        .trigger('drop', { dataTransfer });
+        // DataTranfer vai ajudar o Cypress a simular a ação de arrastar e soltar
+        const dataTransfer = new DataTransfer();
 
-});
+        cy.get('img[alt="Cypress"]')
+            .trigger('dragstart', { dataTransfer });
+        cy.get('.nodejs figure[draggable="true"]')
+            .trigger('drop', { dataTransfer });
 
-it('deve arrastar o robot para a caixinha do python', () => {
-    cy.visit('https://training-wheels-qaninja.herokuapp.com/drag_and_drop');
+    });
 
-    // DataTranfer vai ajudar o Cypress a simular a ação de arrastar e soltar
-    const dataTransfer = new DataTransfer();
+    it('deve arrastar o robot para a caixinha do python', () => {
 
-    cy.get('img[alt="Robot Framework"]')
-        .trigger('dragstart', { dataTransfer });
-    cy.get('.python figure[draggable="true"]')
-        .trigger('drop', { dataTransfer });
+        // DataTranfer vai ajudar o Cypress a simular a ação de arrastar e soltar
+        const dataTransfer = new DataTransfer();
+
+        cy.get('img[alt="Robot Framework"]')
+            .trigger('dragstart', { dataTransfer });
+        cy.get('.python figure[draggable="true"]')
+            .trigger('drop', { dataTransfer });
+
+    });
 
 });
