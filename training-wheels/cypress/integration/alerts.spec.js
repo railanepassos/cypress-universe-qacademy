1834ba708a0e1dad8a72a6c2d1b692bac6fdb7e7
Railane Passos <railanepassos@gmail.com>
2022-06-28 14:51:49 -0300

    Aula: Javascript Prompt Popup com Stubs

--- training-wheels/cypress/integration/alerts.spec.js
+++ training-wheels/cypress/integration/alerts.spec.js
@@ -34,3 +34,24 @@ it('deve cancelar a solicitação', () => {
   cy.get('p#result').should('have.text', 'Mensagem não confirmada');
 
 });
+
+it('deve exibir mensagem de boas vindas com meu nome', () => {
+  /**
+   * Para automatizar este cenário precisaremos ultilizar um Stub, pois quando o prompt é aberto é esperado que o campo seja preenchido e enquanto não for preenchido os passos não são concluidos.
+   * 
+   * Stubs são funções ou métodos que substituem comportamentos ou até mesmo trechos de código. Vamos ultilizar um stub para subistituir o comportamento relacionado a preenchimento do prompt simulando seu a ação.
+   * 
+   */
+
+  cy.visit('/javascript_alerts');
+
+  cy.window().then(($win) => {
+    cy.stub($win, 'prompt').returns('Railane');
+    cy.contains('button', 'Prompt').click();
+
+  });
+
+  cy.get('p#result').should('have.text', 'Olá, Railane');
+
+
+});
