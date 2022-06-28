c91df140bed6ce490c87bf487fec166d293d8cb4
Railane Passos <railanepassos@gmail.com>
2022-06-28 14:18:38 -0300

    Aula: Javascript Confirm Popup

--- training-wheels/cypress/integration/alerts.spec.js
+++ training-wheels/cypress/integration/alerts.spec.js
@@ -12,3 +12,25 @@ it('deve exibir uma mensagem de alerta', () => {
   cy.contains('p#result', 'Você ativou um alerta JS');
 
 });
+
+it('deve confirmar a solicitação', () => {
+
+  cy.visit('/javascript_alerts');
+
+  cy.contains('button', 'Confirma').click();
+  cy.on('window:confirm', () => true);
+
+  cy.get('p#result').should('have.text', 'Mensagem confirmada');
+
+});
+
+it('deve cancelar a solicitação', () => {
+
+  cy.visit('/javascript_alerts');
+
+  cy.contains('button', 'Confirma').click();
+  cy.on('window:confirm', () => false);
+
+  cy.get('p#result').should('have.text', 'Mensagem não confirmada');
+
+});
