f2a0a8c7ba24e74103f79365c65473c7dc7e8276
Railane Passos <railanepassos@gmail.com>
2023-04-17 17:08:26 -0300

    Configuração de plugin e inclusão de novo teste

--- samuraibs/tests/cypress.config.js
+++ samuraibs/tests/cypress.config.js
@@ -1,12 +1,36 @@
 const { defineConfig } = require("cypress");
+const { Pool } = require('pg');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      const pool = new Pool({
+        host: 'trumpet.db.elephantsql.com',
+        user: 'yytbngih',
+        password: 'obS78oVT2S45dtgquU6XM_Ha9v__6DdV',
+        database: 'yytbngih',
+        port: 5432
+      });
+
+      on('task', {
+        removeUser(email) {
+          return new Promise((resolve) => {
+            pool.query('DELETE FROM public.users WHERE email = $1', [email], (error, result) => {
+              if (error) {
+                throw error
+              }
+              resolve({ success: result })
+            })
+          })
+        }
+      })
+
     },
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1440,
     viewportHeight: 900,
+
   },
 });
--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -38,7 +38,7 @@ it('deve cadatrar um novo usuário através do acesso via pagina de login', () =
 
 */
 
-it('deve simular cadastro com sucesso ao interceptar chamadas', () => {
+it('deve simular cadastro de usuário com sucesso ao interceptar chamadas', () => {
   // Os testes usando o faker infla o banco de dados. Esse caso de teste evita esse efeito colateral. Dessa forma não temos um teste integrado pois estamos maquiando o backend porem nesse momento devemos testar apénas o front.
 
   let userData = {
@@ -61,6 +61,35 @@ it('deve simular cadastro com sucesso ao interceptar chamadas', () => {
   cy.contains('button', 'Cadastrar').click();
 
   cy.wait('@postUser');
+
+  cy.get('.toast')
+    .should('be.visible')
+    .find('p')
+    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+
+});
+
+it('deve cadastrar usuário com sucesso após remover usuário do banco de dados', () => {
+
+  let userData = {
+    name: "Maria",
+    email: "maria@mailinator.com",
+    password: "pwd123"
+
+  }
+
+  cy.task('removeUser', userData.email)
+    .then((result) => {
+      console.log(result)
+
+    })
+  cy.visit('/signup');
+
+  cy.get('input[placeholder="Nome"]').type(userData.name);
+  cy.get('input[placeholder="E-mail"]').type(userData.email);
+  cy.get('input[placeholder="Senha"]').type(userData.password);
+
+  cy.contains('button', 'Cadastrar').click();
 
   cy.get('.toast')
     .should('be.visible')
