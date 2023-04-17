5c2c44cb20d21cbbe6354e3e8f38aa8862c1b2ee
Railane Passos <railanepassos@gmail.com>
2023-04-17 17:39:55 -0300

    Organização de testes já existentes e inclusão de novo teste

--- samuraibs/tests/cypress/e2e/signup.cy.js
+++ samuraibs/tests/cypress/e2e/signup.cy.js
@@ -1,99 +1,130 @@
 import { faker } from "@faker-js/faker";
 
 /*
-it('deve cadatrar um novo usuário através do acesso direto a pagina', () => {
+  describe('Cenários de cadastro de usuário ultilizando o faker', () => {
+    it('deve cadatrar um novo usuário através do acesso direto a pagina', () => {
 
-  cy.visit('/signup');
-
-  cy.get('input[placeholder="Nome"]').type(faker.name.fullName());
-  cy.get('input[placeholder="E-mail"]').type(faker.internet.email());
-  cy.get('input[placeholder="Senha"]').type(faker.internet.password(10));
-
-  cy.contains('button', 'Cadastrar').click();
-
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
-
-});
-
-it('deve cadatrar um novo usuário através do acesso via pagina de login', () => {
-
-  cy.visit('/');
-  cy.contains('a', 'Criar conta').click();
-
-  cy.get('input[placeholder="Nome"]').type(faker.name.fullName());
-  cy.get('input[placeholder="E-mail"]').type(faker.internet.email());
-  cy.get('input[placeholder="Senha"]').type(faker.internet.password(10));
-
-  cy.contains('button', 'Cadastrar').click();
-
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
-
-});
-
+      cy.visit('/signup');
+    
+      cy.get('input[placeholder="Nome"]').type(faker.name.fullName());
+      cy.get('input[placeholder="E-mail"]').type(faker.internet.email());
+      cy.get('input[placeholder="Senha"]').type(faker.internet.password(10));
+    
+      cy.contains('button', 'Cadastrar').click();
+    
+      cy.get('.toast')
+        .should('be.visible')
+        .find('p')
+        .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+    
+    });
+    
+    it('deve cadatrar um novo usuário através do acesso via pagina de login', () => {
+    
+      cy.visit('/');
+      cy.contains('a', 'Criar conta').click();
+    
+      cy.get('input[placeholder="Nome"]').type(faker.name.fullName());
+      cy.get('input[placeholder="E-mail"]').type(faker.internet.email());
+      cy.get('input[placeholder="Senha"]').type(faker.internet.password(10));
+    
+      cy.contains('button', 'Cadastrar').click();
+    
+      cy.get('.toast')
+        .should('be.visible')
+        .find('p')
+        .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+    
+    });
+  }) 
 */
 
-it('deve simular cadastro de usuário com sucesso ao interceptar chamadas', () => {
-  // Os testes usando o faker infla o banco de dados. Esse caso de teste evita esse efeito colateral. Dessa forma não temos um teste integrado pois estamos maquiando o backend porem nesse momento devemos testar apénas o front.
+// Testes usando o faker infla o banco de dados. Para evitar esse efeito colateral existem algumas alternativas, porem dessa forma não temos um teste integrado pois estamos maquiando o backend porem se o objetivo é testar apénas o front pode ser feito como no exemplo abaixo.
+describe('Cenários de cadastro de usuário interceptando chamada', () => {
+  it('deve simular cadastro de usuário com sucesso ao interceptar chamadas', () => {
 
-  let userData = {
-    name: "Maria",
-    email: "maria@mailinator.com",
-    password: "pwd123"
+    let userData = {
+      name: "Maria",
+      email: "maria@mailinator.com",
+      password: "pwd123"
 
-  }
+    }
 
-  cy.visit('/signup');
+    cy.visit('/signup');
 
-  cy.get('input[placeholder="Nome"]').type(userData.name);
-  cy.get('input[placeholder="E-mail"]').type(userData.email);
-  cy.get('input[placeholder="Senha"]').type(userData.password);
+    cy.get('input[placeholder="Nome"]').type(userData.name);
+    cy.get('input[placeholder="E-mail"]').type(userData.email);
+    cy.get('input[placeholder="Senha"]').type(userData.password);
 
-  cy.intercept('POST', '/users', {
-    statusCode: 200
-  }).as('postUser')
+    cy.intercept('POST', '/users', {
+      statusCode: 200
+    }).as('postUser')
 
-  cy.contains('button', 'Cadastrar').click();
+    cy.contains('button', 'Cadastrar').click();
 
-  cy.wait('@postUser');
+    cy.wait('@postUser');
 
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
 
-});
+  });
 
-it('deve cadastrar usuário com sucesso após remover usuário do banco de dados', () => {
+})
 
-  let userData = {
-    name: "Maria",
-    email: "maria@mailinator.com",
-    password: "pwd123"
+describe('Cenários com interdependência de cadastro de usuário e usuário já cadastrado', () => {
+  it('deve cadastrar usuário com sucesso após remover usuário do banco de dados', () => {
 
-  }
+    let userData = {
+      name: "Maria",
+      email: "maria@mailinator.com",
+      password: "pwd123"
 
-  cy.task('removeUser', userData.email)
-    .then((result) => {
-      console.log(result)
+    }
 
-    })
-  cy.visit('/signup');
+    cy.task('removeUser', userData.email)
+      .then((result) => {
+        console.log(result)
 
-  cy.get('input[placeholder="Nome"]').type(userData.name);
-  cy.get('input[placeholder="E-mail"]').type(userData.email);
-  cy.get('input[placeholder="Senha"]').type(userData.password);
+      })
+    cy.visit('/signup');
 
-  cy.contains('button', 'Cadastrar').click();
+    cy.get('input[placeholder="Nome"]').type(userData.name);
+    cy.get('input[placeholder="E-mail"]').type(userData.email);
+    cy.get('input[placeholder="Senha"]').type(userData.password);
 
-  cy.get('.toast')
-    .should('be.visible')
-    .find('p')
-    .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+    cy.contains('button', 'Cadastrar').click();
 
-});
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!');
+
+  });
+
+  it('deve exibir mensagem para e-mail já cadastrado', () => {
+
+    let userData = {
+      name: "Maria",
+      email: "maria@mailinator.com",
+      password: "pwd123"
+
+    }
+
+    cy.visit('/signup');
+
+    cy.get('input[placeholder="Nome"]').type(userData.name);
+    cy.get('input[placeholder="E-mail"]').type(userData.email);
+    cy.get('input[placeholder="Senha"]').type(userData.password);
+
+    cy.contains('button', 'Cadastrar').click();
+
+    cy.get('.toast')
+      .should('be.visible')
+      .find('p')
+      .should('have.text', 'Email já cadastrado para outro usuário.');
+
+  });
+
+})
