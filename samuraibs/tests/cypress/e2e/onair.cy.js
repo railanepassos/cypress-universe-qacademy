a991feec2fb40780fe53373159b81708688d950c
Railane Passos <railanepassos@gmail.com>
2023-04-17 13:13:47 -0300

    Inclusão de novo teste para verificar responsividade

--- samuraibs/tests/cypress/e2e/onair.cy.js
+++ samuraibs/tests/cypress/e2e/onair.cy.js
@@ -1,3 +1,5 @@
+const sizes = ['iphone-6', 'ipad-2', [1024, 768]]
+
 it('webapp deve estar online', () => {
 
   cy.visit('/');
@@ -5,3 +7,19 @@ it('webapp deve estar online', () => {
     .should('eq', 'Samurai Barbershop by QAninja')
 
 });
+
+sizes.forEach((size) => {
+  // make assertions on the logo using
+  // an array of different viewports
+
+  it(`logo deve ser visivel na tela com tamanho ${size}`, () => {
+    if (Cypress._.isArray(size)) {
+      cy.viewport(size[0], size[1])
+    } else {
+      cy.viewport(size)
+    }
+
+    cy.visit('/')
+    cy.get('.logo').should('be.visible')
+  })
+})
